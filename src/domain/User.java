2aa71e743eb611c24c547f95735ed8d91b698663
Hank <l945457758@gmail.com>
2018-06-03 08:45:38 +0800

    Update

---
+++ src/domain/User.java
@@ -0,0 +1,51 @@
+package domain;
+
+public class User {
+    private String username;
+    private String password;
+    private String phone;
+    private String email;
+    private String verifyCode;
+
+    /*public User(String username, String password, String phone, String email){
+        this.username = username;
+        this.password = password;
+        this.phone = phone;
+        this.email = email;
+    }*/
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setVerifyCode(String verifyCode) {
+        this.verifyCode = verifyCode;
+    }
+    public String getVerifyCode() {
+        return verifyCode;
+    }
+}
